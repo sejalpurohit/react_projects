63645b10837c3c7705eb42960576d57d5467a16d
sejal <sejalpurohit3195@gmail.com>
2024-10-23 14:57:12 +0100

    search updated

--- FoodVilla/src/components/Body.js
+++ FoodVilla/src/components/Body.js
@@ -1,18 +1,46 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../constant";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 //check why curly braced{}  are not working or logic in multiline is not working
 function filterData(searchText, restaurants) {
   const filterData = restaurants.filter((restaurant) =>
-    restaurant.info.name.includes(searchText)
+    restaurant?.info?.name?.toLowerCase()?.includes(searchText.toLowerCase())
   );
   return filterData;
 }
+
 const Body = () => {
   const [searchText, setSearchText] = useState("");
-  const [restaurants, setRestaurants] = useState(restaurantList);
-  return (
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [allRestaurants, setallRestaurants] = useState([]);
+
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.1685786&lng=79.9338798&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    setFilteredRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
+    setallRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  }
+
+  useEffect(() => {
+    //make API call here
+    getRestaurants();
+  }, []);
+
+  if(!allRestaurants) return null;
+
+  if(filteredRestaurants.length ===0) return <h1>NO Restaurants found for your match</h1>
+
+  return  (filteredRestaurants.length === 0) ? <Shimmer /> : (
     <>
       <div className="search-container">
         <input
@@ -27,8 +55,8 @@ const Body = () => {
         <button
           className="search-button"
           onClick={() => {
-            const data = filterData(searchText, restaurants);
-            setRestaurants(data);
+            const data = filterData(searchText, allRestaurants);
+            setFilteredRestaurants(data);
 
             //need to filter the data
             //update the state- restaurant List
@@ -40,7 +68,9 @@ const Body = () => {
       </div>
 
       <div className="restaurant-List">
-        {restaurants.map((restuarant) => {
+
+        {/* {no restaurant find logic} */ }
+        {filteredRestaurants.map((restuarant) => {
           return (
             <RestaurantCard {...restuarant.info} key={restuarant.info.id} />
           );
