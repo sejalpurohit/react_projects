189f8aca76e102846890b2a85b2dbff356a46806
sejal <sejalpurohit3195@gmail.com>
2024-10-29 14:32:17 +0000

    shimmer effect

--- FoodVilla/src/components/Shimmer.js
+++ FoodVilla/src/components/Shimmer.js
@@ -1,8 +1,17 @@
-const Shimmer =() =>{
-    return (
+const Shimmer = () => {
+  return (
+    <>
+      <div className="restaurant-List">
+        {Array(50)
+          .fill("")
+          .map((e, index) => (
+            <div  key={index} className="shimmer-card"></div>
+          ))}
+      </div>
 
-        <h1>Loading............</h1>
-    )
-}
+      <h1>Loading............</h1>
+    </>
+  );
+};
 
 export default Shimmer;
